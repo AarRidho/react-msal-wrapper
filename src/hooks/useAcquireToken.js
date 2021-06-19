5c4a3efa11f49f15aac1ca4e9cdff3e456c90d48
Ridho Aru <chrono.legionaires@gmail.com>
2021-06-19 15:53:35 +0700

    update requestRefreshToken

--- src/hooks/useAcquireToken.js
+++ src/hooks/useAcquireToken.js
@@ -1,35 +1,70 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useMsal } from '@azure/msal-react';
 
-function useAcquireToken({ scopes = ['User.Read'], account = null }) {
+function useAcquireToken({
+  scopes = ['User.Read'],
+  account = null,
+  requestRefreshToken = false
+}) {
   const { instance, inProgress, accounts } = useMsal();
   const [accessToken, setAccessToken] = useState(null);
 
   const getData = useCallback(async () => {
     if (inProgress === 'none' && (account || accounts.length > 0)) {
       // Retrieve an access token
-      try {
-        const response = await instance.acquireTokenSilent({
-          account: account || accounts[0],
-          scopes
-        });
+      const request = {
+        account: account || accounts[0],
+        scopes
+      };
 
-        if (response.accessToken) {
-          // console.info({ response });
-          setAccessToken(response.accessToken);
-          return response.accessToken;
+      try {
+        const response = await instance.acquireTokenSilent(request);
+
+        checkTokenResponse(response);
+        return;
+      } catch (error) {
+        console.log(error?.message, error?.errorCode);
+        if (
+          requestRefreshToken &&
+          (error.errorCode === 'consent_required' ||
+            error.errorCode === 'interaction_required' ||
+            error.errorCode === 'login_required' ||
+            error.errorCode === 'monitor_window_timeout')
+        ) {
+          try {
+            const response = await instance.acquireTokenRedirect(request);
+            return checkTokenResponse(response);
+          } catch {
+            if (accessToken) setAccessToken(null);
+            return null;
+          }
         }
 
-        // console.info({ response });
-        setAccessToken(null);
-        return null;
-      } catch (error) {
-        // console.log(error);
         if (accessToken) setAccessToken(null);
         return null;
       }
     }
-  }, [accessToken, account, accounts, inProgress, instance, scopes]);
+  }, [
+    accessToken,
+    account,
+    accounts,
+    inProgress,
+    instance,
+    requestRefreshToken,
+    scopes
+  ]);
+
+  const checkTokenResponse = (response) => {
+    console.info({ response });
+
+    if (response.accessToken) {
+      setAccessToken(response.accessToken);
+      return response.accessToken;
+    }
+
+    setAccessToken(null);
+    return null;
+  };
 
   useEffect(() => {
     getData();
