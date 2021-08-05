0a7f3245b2025ca73efd393a65ded8a1e406a763
Aar Ridho <chrono.legionaires@gmail.com>
2021-08-05 16:15:09 +0700

    Update acquireToken

--- src/hooks/useGraphPhoto.js
+++ src/hooks/useGraphPhoto.js
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useAcquireToken } from 'react-msal-wrapper';
+import useAcquireToken from './useAcquireToken';
 
 function useGraphPhoto({
   scopes = ['User.Read'],
