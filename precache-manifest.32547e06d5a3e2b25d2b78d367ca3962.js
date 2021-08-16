34b3a5b70166b8cb219b6189438e6345bd835fea
Ridho Aru <chrono.legionaires@gmail.com>
2021-08-16 11:50:53 +0700

    Updates

---
+++ precache-manifest.32547e06d5a3e2b25d2b78d367ca3962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6094d39cdfd693c836c1e58bda2cf476",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46f7b92cfe84827b5fcf",
+    "url": "./static/css/main.1f609a92.chunk.css"
+  },
+  {
+    "revision": "a4411b1bff56186e83d0",
+    "url": "./static/js/2.4ed9f19b.chunk.js"
+  },
+  {
+    "revision": "82d992827fa429ee986f35080fb28c57",
+    "url": "./static/js/2.4ed9f19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f7b92cfe84827b5fcf",
+    "url": "./static/js/main.027bcd1d.chunk.js"
+  },
+  {
+    "revision": "793caa3facf8da406fa5",
+    "url": "./static/js/runtime-main.6dca2914.js"
+  }
+]);
