ad60841e320120d26d2e6e8204874d349504a7c9
Aar Ridho <chrono.legionaires@gmail.com>
2021-09-30 22:45:58 +0700

    update abort controller

--- src/hooks/useAcquireToken.js
+++ src/hooks/useAcquireToken.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState, useRef } from 'react';
 import { useMsal } from '@azure/msal-react';
 import {
   InteractionRequiredAuthError,
@@ -13,62 +13,60 @@ function useAcquireToken({
 }) {
   const { instance, accounts, inProgress } = useMsal();
   const [accessToken, setAccessToken] = useState(null);
+  const abortController = useRef(new AbortController());
 
-  const getData = useCallback(
-    async (aborted) => {
-      if (
-        !accessToken &&
-        inProgress === InteractionStatus.None &&
-        (account || accounts.length > 0)
-      ) {
-        // Retrieve an access token
-        const request = {
-          account: account ?? accounts[0],
-          scopes,
-          prompt
-        };
+  const getData = useCallback(async () => {
+    if (
+      !accessToken &&
+      inProgress === InteractionStatus.None &&
+      (account || accounts.length > 0)
+    ) {
+      // Retrieve an access token
+      const request = {
+        account: account ?? accounts[0],
+        scopes,
+        prompt
+      };
 
-        try {
-          const response = await instance.acquireTokenSilent(request);
-          return checkTokenResponse(response, aborted);
-        } catch (error) {
-          // console.log(error?.message, error?.errorCode);
-          if (
-            requestRefreshToken &&
-            (error.errorCode === 'consent_required' ||
-              error.errorCode === 'interaction_required' ||
-              error.errorCode === 'login_required' ||
-              error.errorCode === 'monitor_window_timeout' ||
-              error instanceof InteractionRequiredAuthError)
-          ) {
-            try {
-              const response = await instance.acquireTokenPopup(request);
-              return checkTokenResponse(response, aborted);
-              // eslint-disable-next-line no-empty
-            } catch (errorTokenPopup) {
-              console.log(error);
-            }
+      try {
+        const response = await instance.acquireTokenSilent(request);
+        return checkTokenResponse(response, abortController);
+      } catch (error) {
+        // console.log(error?.message, error?.errorCode);
+        if (
+          requestRefreshToken &&
+          (error.errorCode === 'consent_required' ||
+            error.errorCode === 'interaction_required' ||
+            error.errorCode === 'login_required' ||
+            error.errorCode === 'monitor_window_timeout' ||
+            error instanceof InteractionRequiredAuthError)
+        ) {
+          try {
+            const response = await instance.acquireTokenPopup(request);
+            return checkTokenResponse(response, abortController);
+            // eslint-disable-next-line no-empty
+          } catch (errorTokenPopup) {
+            console.log(error);
           }
-
-          if (accessToken) setAccessToken(null);
-          return null;
         }
-      }
-    },
-    [
-      accessToken,
-      account,
-      accounts,
-      inProgress,
-      instance,
-      prompt,
-      requestRefreshToken,
-      scopes
-    ]
-  );
 
-  const checkTokenResponse = (response, aborted) => {
-    if (aborted) return;
+        if (accessToken) setAccessToken(null);
+        return null;
+      }
+    }
+  }, [
+    accessToken,
+    account,
+    accounts,
+    inProgress,
+    instance,
+    prompt,
+    requestRefreshToken,
+    scopes
+  ]);
+
+  const checkTokenResponse = (response, abortController) => {
+    if (abortController.current.signal.aborted) return;
 
     if (response.accessToken) {
       setAccessToken(response.accessToken);
@@ -80,11 +78,11 @@ function useAcquireToken({
   };
 
   useEffect(() => {
-    let aborted = false;
-    getData(aborted);
+    const AbortController = abortController.current;
+    getData();
 
     return () => {
-      aborted = true;
+      AbortController.abort();
     };
   }, [getData]);
 
